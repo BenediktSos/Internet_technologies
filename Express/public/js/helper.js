22a56111d42499e304b44cbfb852a0b418b751ba
benedikts <sosnowskybene@gmail.com>
2022-04-30 15:28:06 +0200

    interhtmlcommunicationsprotokoll "ihcp"

--- Express/public/js/helper.js
+++ Express/public/js/helper.js
@@ -1,3 +1,6 @@
+
+
+
 greeting = function(){
     alert("Hi Dad!")
 }
@@ -9,4 +12,20 @@ answer = function(){
 invisible = function(){
     var element = document.getElementById("invisible")
     element.style.visibility = "hidden"
+}
+
+
+setValues = function(){
+    const url = document.URL
+    const urlSearchParams = new URLSearchParams(url)
+
+    
+    const sender = urlSearchParams.get('sender');
+    const reciever = urlSearchParams.get('reciever');
+    const message = urlSearchParams.get('message');
+
+
+    document.getElementById("sender_display").innerHTML = sender
+    document.getElementById("reciever_display").innerHTML = reciever
+    document.getElementById("message_display").innerHTML = message
 }
--- Express/simpleBackend.js
+++ Express/simpleBackend.js
@@ -1,13 +1,14 @@
 'use strict'
 const express = require('express');
+const { dirname } = require('path');
 const app = express();
+const path = require('path')
 app.use(express.static('public'))
 
 // Wir laden contents aus dem public Ordner
 app.use('/css', express.static(__dirname + '/public/css'))
 app.use('/js', express.static(__dirname + '/public/js'))
-app.use('/images', express.static(__dirname + '/public/images'))
-
+app.use('/media', express.static(__dirname + '/public/media'))
 
 
 /**
@@ -22,27 +23,11 @@ function startServer() {
 }
 
 app.get('/request', function (req, res) {
-    res.sendFile("/home/benedikts/Hochschule/2.Semester/Internettechnologien/Block_1/Internet_technologies/Express/public/request.html")
-    
+    res.sendFile(__dirname + "/public/request.html")
 })
     
 app.get('/answer', function (req, res) {
-    let sender = req.query.sender
-    let reciever = res.query.reciever
-    let message = req.query.message
-
-    let sender_display = document.getElementById("sender_display")
-    let reciever_display = document.getElementById("reciever_display")
-    let message_display = document.getElementById("message_display")
-    
-    sender_display.innerHTML = sender
-    reciever_display.innerText = reciever
-    message_display.innerText = message
-
-    res.send(sender_display)
-
-    res.sendFile("/home/benedikts/Hochschule/2.Semester/Internettechnologien/Block_1/Internet_technologies/Express/public/answer.html")
+    res.sendFile(__dirname + "/public/answer.html")
 })
     
 startServer()
-    
